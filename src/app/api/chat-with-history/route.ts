d8972060b35a49b78714d2fb3336d5fe72eba8af
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-04-16 16:24:05 +0900

    「14. エクササイズ.02 履歴付属チャット」の変更分

---
+++ src/app/api/chat-with-history/route.ts
@@ -0,0 +1,59 @@
+import { PromptTemplate } from '@langchain/core/prompts';
+import { ChatOpenAI } from '@langchain/openai';
+import { Message as VercelChatMessage, LangChainAdapter } from 'ai';
+ 
+export const runtime = 'edge'
+ 
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
+ 
+const TEMPLATE = `あなたはコメディアンです。ユーザの質問に機知に富んだ返答をし、ジョークを言います。
+ 
+Current conversation:
+{chat_history}
+ 
+user: {input}
+assistant:`;
+ 
+export async function POST(req: Request) {
+  try {
+    const { messages } = await req.json();
+ 
+    const formattedPreviousMessages = messages
+      .slice(0, -1)
+      .map(formatMessage);
+ 
+    const currentMessageContent = messages.at(-1).content;
+ 
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+ 
+    const model = new ChatOpenAI({
+      apiKey: process.env.OPENAI_API_KEY!,
+      model: 'gpt-4o',
+      temperature: 0.6,
+      verbose: true,
+    });
+ 
+    const chain = prompt.pipe(model.bind({ stop: ['?'] }));
+ 
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join('\n'),
+      input: currentMessageContent,
+    });
+ 
+    return LangChainAdapter.toDataStreamResponse(stream);
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+ 
+    return new Response(
+      JSON.stringify({ error: 'Unknown error occurred' }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } },
+    );
+  }
+}
