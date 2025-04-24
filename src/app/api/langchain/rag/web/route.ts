65ac2269e5b84b52b803401bda7aabc08b303c06
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-04-24 12:09:32 +0900

    質問に対してweb検索を行い、結果をvectolにして保存したベクトルデータから質問文に類似したテキストを抜き出し質問に答える

--- src/app/api/langchain/rag/web/route.ts
+++ src/app/api/langchain/rag/web/route.ts
@@ -24,6 +24,9 @@ assistant:`;
 
 /**
  * チャット応答AI（記憶・モデル変更対応済み）
+ * 質問に対してweb検索を行い、結果をvectolにして保存
+ * 保存したベクトルデータから質問文に類似したテキストを抜き出し質問に答える
+ * 
  * @param req 
  * @returns 
  */
@@ -40,9 +43,18 @@ export async function POST(req: Request) {
     // Tavilyツールの準備
   const tavily = new TavilySearchAPIRetriever({
     apiKey: process.env.TAVILY_API_KEY,
-    k: 5,
+    k: 10,
     includeGeneratedAnswer: true,
   });
+
+  // ドキュメントを分割 & ベクトル化
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 200,
+    chunkOverlap: 50,
+  });
+  
+  // 埋め込みモデル
+  const embeddings = new OpenAIEmbeddings({apiKey: process.env.OPENAI_API_KEY});
 
 
     // 過去の履歴{chat_history}
@@ -76,18 +88,48 @@ export async function POST(req: Request) {
           ],
         });
     }
+
         
     // const prompt = PromptTemplate.fromTemplate(TEMPLATE);
     const chain = RunnableSequence.from([
       async ({history, input}: {history: string; input:string}) => {
+        // 記事の取得
         const docs = await tavily.invoke(input);
-        const context = docs.map((d: any) => d.pageContent).join("\n---\n");
+
+        // 記事の分割
+        console.log(docs.length);
+        const splitDocs = await splitter.splitDocuments(docs);
+        console.log(splitDocs.length);
+
+        // 分割して保存
+        const vectorStore = await MemoryVectorStore.fromDocuments(
+          splitDocs,
+          embeddings
+        );
+
+        // Retriever を用いて、Vectore Store から検索し返答
+        //const retriever = vectorStore.asRetriever({ k: 2 });
+        // const response = await retriever.invoke(input);
+        
+        // vectore storeからinputで検索
+        const response = await vectorStore.similaritySearchWithScore(input, 2);
+        
+        // 抽出
+        const context = response.map(([doc, score]) => ({
+          content: doc.pageContent,
+          score: score,
+        }));
+        const data = context.map(item => item.content);
+        
+        console.log(response);
+        // console.log(JSON.stringify(context));
+
         const chat_history = history;
         return `3行にまとめて回答してください。
           Current conversation:
           ${chat_history}
  
-          情報: ${context}
+          情報: ${data}
           これらの情報をもとにユーザーに答えてください。
 
           質問: ${input}
