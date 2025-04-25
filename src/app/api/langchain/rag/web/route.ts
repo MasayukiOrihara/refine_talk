2d448cc006a89a98f8c543fee8fd366217c57128
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-04-25 13:51:56 +0900

    無駄なコメントアウトの消去

--- src/app/api/langchain/rag/web/route.ts
+++ src/app/api/langchain/rag/web/route.ts
@@ -1,6 +1,5 @@
 import { ChatAnthropic } from '@langchain/anthropic';
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { FakeListChatModel } from '@langchain/core/utils/testing';
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import { Message as VercelChatMessage, LangChainAdapter } from 'ai';
@@ -14,18 +13,6 @@ import { PineconeStore } from "@langchain/pinecone";
 const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
-
-/** AstraDBの準備 */
-// const {ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT} = process.env;
-// if (!ASTRA_DB_APPLICATION_TOKEN || !ASTRA_DB_API_ENDPOINT) {
-//   throw new Error("Astra DB の環境変数が未設定です。");
-// }
-// const astraConfig = {
-//   token: ASTRA_DB_APPLICATION_TOKEN,
-//   endpoint: ASTRA_DB_API_ENDPOINT,
-//   collection: "web_4",
-//   skipCollectionProvisioning: false,
-// };
 
 /**
  * チャット応答AI（記憶なし・モデル変更対応済み）
@@ -46,29 +33,23 @@ export async function POST(req: Request) {
      * tavilyでのweb検索
      */
     // Tavilyツールの準備
-  const tavily = new TavilySearchAPIRetriever({
-    apiKey: process.env.TAVILY_API_KEY,
-    k: 5,
-    includeGeneratedAnswer: true,
-  });
+    const tavily = new TavilySearchAPIRetriever({
+      apiKey: process.env.TAVILY_API_KEY,
+      k: 5,
+      includeGeneratedAnswer: true,
+    });
 
-  // ドキュメントを分割 & ベクトル化
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 200,
-    chunkOverlap: 50,
-  });
+    // ドキュメントを分割 & ベクトル化
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 200,
+      chunkOverlap: 50,
+    });
   
-  // 埋め込みモデル
-  const embeddings = new OpenAIEmbeddings({
-    modelName: "text-embedding-3-small",
-    apiKey: process.env.OPENAI_API_KEY
-  });
-
-
-    // 過去の履歴{chat_history}
-    const formattedPreviousMessages = messages
-      .slice(0, -1)
-      .map(formatMessage)
+    // 埋め込みモデル
+    const embeddings = new OpenAIEmbeddings({
+      modelName: "text-embedding-3-small",
+      apiKey: process.env.OPENAI_API_KEY
+    });
 
     // メッセージ{input}
     const currentMessageContent = messages[messages.length - 1].content;
@@ -98,9 +79,8 @@ export async function POST(req: Request) {
     }
 
         
-    // const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+    /** chainの基本形式？ */
     const chain = RunnableSequence.from([
-       // async ({history, input}: {history: string; input:string}) => {
       async (input: string) => {
         // 記事の取得
         const docs = await tavily.invoke(input);
@@ -109,63 +89,9 @@ export async function POST(req: Request) {
         const splitDocs = await splitter.splitDocuments(docs);
         console.log("分割数: " + splitDocs.length);
 
-        // 分割して保存
-        // const vectorStore = await MemoryVectorStore.fromDocuments(
-        //   splitDocs,
-        //   embeddings
-        // );
-
-
-      //   try {
-      //     vectorStore = await FaissStore.fromDocuments(
-      //       splitDocs,
-      //       embeddings
-      //     );
-      //await vectorStore.save("src/data");
-      // } catch (e) {
-      //   console.log(e);
-      // }
-
-      // 追加したいテスト文書
-// const docsTest = [
-//   new Document({
-//     pageContent: "This is the first test document.",
-//     metadata: { source: "test1" },
-//   }),
-//   new Document({
-//     pageContent: "Another example for vector storage.",
-//     metadata: { source: "test2" },
-//   }),
-// ];
-
-
-      //console.log(astraConfig);
-     // console.log(splitDocs);
-        // DBへ接続
-        // let vectorStore = await AstraDBVectorStore.fromDocuments(
-        //   docsTest,
-        //   embeddings,
-        //   astraConfig
-        // );
-        // await vectorStore.addDocuments(docsTest);
-      
-          
-
-        // HNSWLibを試す
-        // const indexPath = "./hnsw_index";
-        // // 初回作成＆保存
-        // let vectorStore = null;
-        // try {
-        //   vectorStore = await HNSWLib.fromDocuments(splitDocs, embeddings);
-        //   await vectorStore.save(indexPath); // 保存される
-        // } catch (e){
-        //   console.log(e);
-        // }
-
-
-        // pineconeを試す
-
-        // Pinecone の初期化
+        /**
+         *  Pineconeを試してみる
+         */
         const pinecone = new Pinecone({
           apiKey: process.env.PINECONE_API_KEY!
         });
@@ -173,16 +99,13 @@ export async function POST(req: Request) {
         // インデックスを取得
         const index = pinecone.index(process.env.PINECONE_INDEX!);
 
-        // Retriever を用いて、Vectore Store から検索し返答
-        //const retriever = vectorStore.asRetriever({ k: 2 });
-        // const response = await retriever.invoke(input);
-
         // PineconeStore を作成し、ドキュメントを追加
         const vectorStore = await PineconeStore.fromDocuments(splitDocs, embeddings, {
           pineconeIndex: index,
           namespace: "default", // 必要に応じて名前空間を設定
         });
         
+
         // vectore storeからinputで検索
         const response = await vectorStore.similaritySearchWithScore(input, 2);
         
@@ -192,12 +115,8 @@ export async function POST(req: Request) {
           score: score,
         }));
         const data = context.map(item => item.content);
-        
         console.log(response);
-        // console.log(JSON.stringify(context));
 
-        // const chat_history = history;
-        // Current conversation: ${chat_history}
         return `3行にまとめて回答してください。
 
           情報: ${data}
@@ -209,7 +128,7 @@ export async function POST(req: Request) {
       (prompt: string) => model.invoke(prompt),
     ]);
 
-
+    // ストリーム指定はしてるけど
     const stream = await chain.stream(currentMessageContent);
 
     return LangChainAdapter.toDataStreamResponse(stream);
