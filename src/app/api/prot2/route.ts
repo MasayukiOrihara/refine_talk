1d09c807d2228c5d3f6135ebee7caf02368e8cdb
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-05-02 17:51:30 +0900

    報連相ワークAIに着手。ガワだけ書いてある。

--- src/app/api/prot2/route.ts
+++ src/app/api/prot2/route.ts
@@ -1,25 +1,194 @@
-import { openai } from '@ai-sdk/openai';
-import { ChatAnthropic } from '@langchain/anthropic';
+import { PromptTemplateJson } from '@/contents/type';
+import { isObject, loadJsonFile } from '@/contents/utils';
+import { AIMessage, BaseMessage, HumanMessage } from '@langchain/core/messages';
 import { PromptTemplate } from '@langchain/core/prompts';
-import { FakeListChatModel } from '@langchain/core/utils/testing';
+import { Annotation, messagesStateReducer, StateGraph } from '@langchain/langgraph';
 import { ChatOpenAI } from '@langchain/openai';
 import { Message as VercelChatMessage, LangChainAdapter } from 'ai';
- 
+
+
+// モデルのセット（OPENAI固定）
+const model = new ChatOpenAI({ apiKey: process.env.OPENAI_API_KEY, model: "gpt-4o", temperature: 0.3 });
+// プロンプトの読み込み
+const template = await loadJsonFile<PromptTemplateJson[]>('src/data/prompt-template.json');
+// プロンプトチェック用関数
+function createErrorResponse(message: string, statusCode: number = 500): Response {
+  return new Response(JSON.stringify({ error: message }), {
+    status: statusCode,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
 // チャット形式
 const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
+// フラグ状態の保持
+let isStartedState = false;
+let isTargetState = false;
+let isReasonState = false;
+let checkTargetState = false;
+let checkReasonState = false;
 
-// テンプレート
-const TEMPLATE = `
-Current conversation:
-{chat_history}
- 
-user: {input}
-assistant:`;
+
+
+/** 初めの状態変更ノード */
+async function setState({ messages }: typeof StateAnnotation.State) {
+  console.log("setState");
+
+  // 現在の状態
+  return {
+    isStarted: isStartedState,
+    isTarget: isTargetState,
+    isReason: isReasonState,
+    checkTarget: checkTargetState,
+    checkReason: checkReasonState
+  }
+}
+
+/** 報連相ワークは始まっているかの状態確認ノード */
+async function isProcessStarted({ messages, isStarted }: typeof StateAnnotation.State) {
+  console.log("isProcessStarted: " + isStarted);
+
+  isStartedState = true;
+}
+
+/** 開発の仕事を想像させるノード */
+async function explainEngineeringTopics({ messages }: typeof StateAnnotation.State) {
+  console.log("explainEngineeringTopics");
+
+  // AIに開発についての話をさせる
+  return {
+    messages: [...messages, new AIMessage("あなたは講師です。開発の仕事について語ってください。\n")],
+  };
+}
+
+/** 1つ目の問題「誰のため」は正解したかの状態確認ノード */
+async function checkTargetMatch({ messages }: typeof StateAnnotation.State) {
+  console.log("checkTargetMatch");
+}
+
+/** 1つ目の問題「誰のため」を聞くノード */
+async function questionTarget({ messages }: typeof StateAnnotation.State) {
+  console.log("questionTarget");
+
+  // 問題を出してもらう
+  messages[messages.length -1].content += "上記について話したのち、下記の質問をしてください。\n[開発の仕事を想定し、報連相は誰のためのものか]";
+  return {
+    messages: [...messages]
+  };
+}
+
+/** 1つ目の問題「誰のため」ヒントノード */
+async function giveTargetHint({ messages }: typeof StateAnnotation.State) {
+  console.log("giveTargetHint");
+
+  // 答えに対してヒントを与える
+  return {
+    messages: [...messages, new AIMessage("ユーザーは答えを外したのであなたはユーザーを諫め、[ヒント]をあげてください。\n")],
+  };
+}
+
+/** 1つ目の問題「誰のため」をクリアしたか状態確認ノード */
+async function isTargetCleared({ messages }: typeof StateAnnotation.State) {
+  console.log("isTargetCleared");
+}
+
+/** 2つ目の問題「なぜリーダーのためなのか」は正解したかの状態確認ノード */
+async function checkReasonMatch({ messages }: typeof StateAnnotation.State) {
+  console.log("checkReasonMatch");
+}
+
+/** 2つ目の問題「なぜリーダーのためなのか」を聞くノード */
+async function questionReason({ messages }: typeof StateAnnotation.State) {
+  console.log("questionReason");
+}
+
+/** 2つ目の問題「なぜリーダーのためなのか」ヒントノード */
+async function giveReasonHint({ messages }: typeof StateAnnotation.State) {
+  console.log("giveReasonHint");
+}
+
+/** 2つ目の問題「なぜリーダーのためなのか」をクリアしたか状態確認ノード */
+async function isReasonCleared({ messages }: typeof StateAnnotation.State) {
+  console.log("isReasonCleared");
+}
+
+/** なぜ報連相が必要になるのかを解説するノード */
+async function explainNewsletter({ messages }: typeof StateAnnotation.State) {
+  console.log("explainNewsletter");
+}
+
+/** 終了前のノード */
+async function exit({ messages }: typeof StateAnnotation.State) {
+  console.log("exit");
+}
+
+/** 結果を保存して終了ノード */
+async function isProcessEnd({ messages }: typeof StateAnnotation.State) {
+  console.log("isProccessEnd");
+}
 
 /**
- * チャット応答AI（記憶・モデル変更対応済み）
+ * グラフ定義
+ * messages: 今までのメッセージを保存しているもの
+ */
+export const StateAnnotation = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    reducer: messagesStateReducer,
+    default: () => [],
+  }),
+    isStarted: Annotation<boolean>({
+      value: (state: boolean = false, action: boolean) => action,
+    }),
+    isTarget: Annotation<boolean>({
+      value: (state: boolean = false, action: boolean) => action,
+    }),
+    isReason: Annotation<boolean>({
+      value: (state: boolean = false, action: boolean) => action,
+    }),
+    checkTarget: Annotation<boolean>({
+      value: (state: boolean = false, action: boolean) => action,
+    }),
+    checkReason: Annotation<boolean>({
+      value: (state: boolean = false, action: boolean) => action,
+    }),
+});
+
+const graph = new StateGraph(StateAnnotation)
+  .addNode("set", setState)
+  .addNode("is1", isProcessStarted)
+  .addNode("is2", isTargetCleared)
+  .addNode("is3", isReasonCleared)
+  .addNode("is4", isProcessEnd)
+  .addNode("check1", checkTargetMatch)
+  .addNode("check2", checkReasonMatch)
+  .addNode("exit", exit)
+  .addNode("explainStart", explainEngineeringTopics)
+  .addNode("explainEnd", explainNewsletter)
+  .addNode("question1", questionTarget)
+  .addNode("question2", questionReason)
+  .addNode("hint1", giveTargetHint)
+  .addNode("hint2", giveReasonHint)
+
+  .addEdge("__start__", "set")
+  .addEdge("set", "is1")
+  .addConditionalEdges("is1", (state) => state.isStarted ? "check1" : "explainStart")
+  .addEdge("explainStart", "question1")
+  .addEdge("question1", "exit")
+  .addConditionalEdges("check1", (state) => state.checkTarget ? "is2" : "hint1")
+  .addEdge("hint1", "question1")
+  .addConditionalEdges("is2", (state) => state.checkReason ? "check2" : "question2")
+  .addEdge("question2", "exit")
+  .addConditionalEdges("check2", (state) => state.checkTarget ? "is3" : "hint2")
+  .addConditionalEdges("is3", (state) => state.checkTarget ? "is4" : "explainEnd")
+  .addEdge("explainEnd", "exit")
+  .addEdge("is4", "exit")
+  .addEdge("exit", "__end__")
+  .compile();
+
+
+
+/**
  * 報連相ワークAI
  * 
  * @param req 
@@ -29,47 +198,38 @@ export async function POST(req: Request) {
   try{
     const body = await req.json();
     const messages = body.messages ?? [];
-    const modelName = body.model ?? 'fake-llm';
 
-    // 過去の履歴{chat_history}
-    const formattedPreviousMessages = messages
-      .slice(0, -1)
-      .map(formatMessage)
-
-    // メッセージ{input}
-    const currentMessageContent = messages[messages.length - 1].content;
-
-    // モデルの指定
-    let model;
-    switch (modelName) {
-      case 'gpt-4o':
-        model = new ChatOpenAI({
-        apiKey: process.env.OPENAI_API_KEY!,
-        model: 'gpt-4o',
-        temperature: 0.9, // ランダム度（高いほど創造的）
-        });
-      break;
-      case 'claude-haiku':
-        model = new ChatAnthropic({
-          model: 'claude-3-5-haiku-20241022',
-          temperature: 0.9, // ランダム度（高いほど創造的）
-        });
-      break;
-      default:
-        model = new FakeListChatModel({
-          responses: [
-            "（応答結果）",
-          ],
-        });
+    // 過去の履歴
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage)
+    // 直近のメッセージを取得
+    const userMessage = messages.at(-1).content;
+    if (!userMessage) {
+      return new Response(JSON.stringify({ error: 'No message provided' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      });
     }
-        
-    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
-    const chain = prompt.pipe(model);
 
-    const stream = await chain.stream({
-      chat_history: formattedPreviousMessages.join('\n'),
-      input: currentMessageContent,
+    // langgraph
+    const result = await graph.invoke({
+      messages: [new HumanMessage(userMessage)],
     });
+
+    console.log("langgraph: " + result.messages[1].content);
+        
+    // テンプレートチェック
+        if (!template.success) return createErrorResponse(template.error);
+        const found = template.data.find(obj => isObject(obj) && obj['name'] === 'api-prot2-question-graph');
+        if (!found) throw new Error('テンプレートが見つかりませんでした');
+    
+        // ストリーミング応答を取得
+        const prompt = PromptTemplate.fromTemplate(found.template);
+        const chain = prompt.pipe(model);
+        const stream = await chain.stream({ 
+          userMessage: userMessage,
+          aiMessage: result.messages[1].content,
+          chatHistory: formattedPreviousMessages
+        });
 
     return LangChainAdapter.toDataStreamResponse(stream);
   } catch (error) {
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -21,7 +21,8 @@ export const Chat: React.FC<ChatProps> = ({ model }) => {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     // APIの読み込み
     //api: 'api/prot1',
-    api: 'api/prot2/question/graph',
+    //api: 'api/prot2/question/graph',
+    api: 'api/prot2',
     //api: 'api/chat',
     // api: 'api/langchain/rag/web',
     //api: 'api/langchain',
