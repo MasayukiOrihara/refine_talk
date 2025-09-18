b10bcb44e3dc93633422cb06465362e7adc17e06
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-09-18 09:08:04 +0900

    ダミーエラーの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import { Footer } from "@/components/footer";
 import { SideMenu } from "@/components/side-menu";
 import { SubPage } from "@/components/sub-page";
 import { ErrorBoundary } from "@/components/error/ErrorBoundary";
-import { BuggyComponent } from "@/components/error/BuggyComponent";
 
 export default function Home() {
   return (
     <ErrorBoundary>
-      <BuggyComponent />
       <div className="h-screen flex flex-col">
         <Header />
         <div className="flex flex-1">
